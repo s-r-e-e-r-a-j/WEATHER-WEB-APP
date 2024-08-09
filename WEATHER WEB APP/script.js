953a8b0dceb07cfbae03b3f1926abbceacc508b9
Sreeraj <154326486+C-O-D-E-G-A-M-E-R@users.noreply.github.com>
2024-08-09 23:47:58 +0530

    Add files via upload

---
+++ WEATHER WEB APP/script.js
@@ -0,0 +1,24 @@
+
+const form=document.querySelector("#form");
+const input=document.querySelector("#input")
+const output=document.querySelector("#output");
+
+form.addEventListener("submit",async(e)=>{
+
+e.preventDefault();
+  output.style.display="block"
+  try{
+
+let api=`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&unit=metric`
+let apikey="2691a8ef6debec9416140d77a76c01b7"
+
+let response=await fetch(api + `&appid=${apikey}`)
+let data=await response.json();
+output.innerText=`Humidity ${data.main.humidity}\nWindSpeed:${data.wind.speed}\nTemperature:${Math.floor(data.main.temp)}\n Location:${data.name} \nStatus : ${data.weather[0].description}`
+     console.log(data)
+  }catch(error){
+    console.log(error)
+  }
+
+
+})
